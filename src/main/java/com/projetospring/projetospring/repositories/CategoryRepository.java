48ed24d550d71e7c711632d00e43d086dce076b9
thaissilva <thaissilva@localhost>
2023-12-13 18:13:25 -0300

    Created Category,CategoryRepository,CategoryResource,CategorySource

---
+++ src/main/java/com/projetospring/projetospring/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.projetospring.projetospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetospring.projetospring.entities.Category;
+
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {	
+}
