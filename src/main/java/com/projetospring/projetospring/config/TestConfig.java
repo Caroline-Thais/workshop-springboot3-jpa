d6c767440e49a73e4bba2bd49b84562fa348c371
thaissilva <thaissilva@localhost>
2023-12-14 15:16:32 -0300

    OrderItem, many-to-many association with extra attributes.

--- src/main/java/com/projetospring/projetospring/config/TestConfig.java
+++ src/main/java/com/projetospring/projetospring/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projetospring.projetospring.entities.Category;
 import com.projetospring.projetospring.entities.Order;
+import com.projetospring.projetospring.entities.OrderItem;
 import com.projetospring.projetospring.entities.Product;
 import com.projetospring.projetospring.entities.User;
 import com.projetospring.projetospring.entities.enums.OrderStatus;
 import com.projetospring.projetospring.repositories.CategoryRepository;
+import com.projetospring.projetospring.repositories.OrderItemRepository;
 import com.projetospring.projetospring.repositories.OrderRepository;
 import com.projetospring.projetospring.repositories.ProductRepository;
 import com.projetospring.projetospring.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -69,8 +74,13 @@ public class TestConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
 	}
-	
 	
 }
---
+++ src/main/java/com/projetospring/projetospring/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.projetospring.projetospring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetospring.projetospring.entities.OrderItem;
+
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {	
+}
